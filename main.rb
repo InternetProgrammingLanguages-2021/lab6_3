61701139a6423fc411fba0e2c09c23f02adea650
ekinohito <zhenyagur9@gmail.com>
2022-01-16 05:30:58 +0300

    complete peace

--- main.rb
+++ main.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
-# @param min_x [Numeric]
-# @param max_x [Numeric]
-# @param iteration_quantity [Numeric]
-# @param func [Proc]
-def intg(min_x, max_x, iteration_quantity = 1000, func = nil, &block)
-  func ||= block
-  min_x = min_x.to_f
-  max_x = max_x.to_f
-  range = max_x - min_x
-  dx = range.to_f / iteration_quantity
-  dx * (min_x...max_x).step(dx).reduce do |acc, x|
-    acc + func.call(x)
+# class containing utils for integral calculation
+class IntegralUtil
+  def self.dx(min_x, max_x, iteration_quantity)
+    min_x = min_x.to_f
+    max_x = max_x.to_f
+    range = max_x - min_x
+    range.to_f / iteration_quantity
+  end
+
+  def self.intg(min_x, max_x, iteration_quantity = 1000, &func)
+    dx = dx(min_x, max_x, iteration_quantity)
+    dx * (min_x.to_f...max_x.to_f).step(dx).reduce do |acc, value|
+      acc + func.call(value)
+    end
   end
 end
--- user.rb
+++ user.rb
@@ -4,6 +4,6 @@ require './main'
 
 puts "Here are some integral computations
 Integral sin(x)/x * dx from 0.1 to 1 (code block):
-#{intg(0.1, 1, 2000) { |x| Math.sin(x) / x }}
+#{IntegralUtil.intg(0.1, 1, 2000) { |x| Math.sin(x) / x }}
 Integral tg(x + 1)/(x + 1) * dx from 1 to 2 (lambda):
-#{intg(1, 2, 2000, ->(x) do Math.tan(x + 1) / (x + 1) end)}"
+#{IntegralUtil.intg(1, 2, 2000, &->(x) do Math.tan(x + 1) / (x + 1) end)}"
